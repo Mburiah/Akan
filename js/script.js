360acf3d7196181431adb611e68565c1fc8bbb7a
Mburiah <bykieswagix5@gmail.com>
2019-10-27 22:29:16 +0300

    semipolished akan naming webapp

---
+++ js/script.js
@@ -0,0 +1,33 @@
+var maleNames = ["Kwasi", "Kwandwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"]
+var femaleNames = ["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"]
+var dayOfTheWeek = ["Sunday", "Monday", "Tuesday", "Wednesday","Thursday", "Friday", "Saturday"]
+
+function giveAkanName () {
+    var year =parseInt(document.getElementById("year").value);
+    var month=parseInt(document.getElementById("month").value);
+    var day=parseInt(document.getElementById("day").value);
+    var gender=getGender();
+    var date = new Date(year +"/" + month + "/var"+day);
+    var dayBorn = date.getDay();
+    var akanName;
+
+    if(gender ==="male"){
+        akanName = maleNames[dayBorn];
+    }
+    else{
+        akanName = femaleNames[dayBorn];
+    }
+    alert("You were born on "+dayOfTheWeek[dayBorn]+ " and your Akan name is "+akanName);
+}
+    function getGender(){
+        var gender = document.getElementsByName("gender");
+
+        for(i = 0; i < gender.length; i++){
+            if(gender[i].checked){
+                return(gender[i].value)
+        }
+    }
+}
+    
+
+    
